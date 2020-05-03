e832d77d5acf6d6732c434397dd3e2596165e21a
Shayesteh Arasti <shayesteharasti@gmail.com>
2020-05-03 11:05:43 +0430

    what is going on

--- frate/settings/local.py
+++ frate/settings/local.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'frate.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'frate',
+        'USER': 'postgres',
+        'PASSWORD': 'frate2020',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
