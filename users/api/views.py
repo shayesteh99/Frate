a634fa8847c2d118d9971cdfa6efc2a7a33608a1
Shayesteh Arasti <shayesteharasti@gmail.com>
2020-05-07 13:20:39 +0430

    change in FrateUser model

--- users/api/views.py
+++ users/api/views.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.models import User
 from rest_framework.generics import CreateAPIView
-# from rest_framework.permissions import IsAdminUser
+from rest_framework.permissions import AllowAny
 
 from users.api.serializers import FUserSerializer
 from users.models import FrateUser
@@ -8,7 +8,11 @@ from users.models import FrateUser
 class FrateUsersView(CreateAPIView):
     queryset = User.objects.all()
     serializer_class = FUserSerializer
+    permission_classes = [AllowAny]
+
     def create(self, request):
     	print("data: ", request.data)
+    	username = request.data['Username']
+    	password = request.data['Password']
 
    
--- users/models.py
+++ users/models.py
@@ -3,4 +3,5 @@ from django.db import models
 # Create your models here.
 
 class FrateUser(models.Model):
-	pass
+	Username = models.EmailField(blank = True)
+	Password = models.CharField(max_length = 20, blank = True)
