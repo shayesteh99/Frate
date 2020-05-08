bf9180de90277e2350a7db72fe3764ba52367a8e
Shayesteh Arasti <shayesteharasti@gmail.com>
2020-05-08 14:49:41 +0430

    what is wrong

--- users/models.py
+++ users/models.py
@@ -3,6 +3,6 @@ from django.db import models
 # Create your models here.
 
 class FrateUser(models.Model):
-	Email = models.EmailField(blank = True)
+	Email = models.EmailField()
 	Username = models.CharField(max_length = 50, blank = True)
 	Password = models.CharField(max_length = 20, blank = True)
