c1a1ccfc22e39600e1f8f6356f736951df432e32
Shayesteh Arasti <shayesteharasti@gmail.com>
2020-05-10 17:36:18 +0430

    fill get method for users

--- users/api/views.py
+++ users/api/views.py
@@ -30,7 +30,8 @@ class FrateUsersView(CreateAPIView):
     	return Response(data = {'Status': 1})
 
     def get(self, request):
+        fusers = [u for u in User.objects.all()]
         from rest_framework.response import Response
-        return Response()
+        return Response(fusers)
 
    
