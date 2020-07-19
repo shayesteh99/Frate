4324f3471a2766b882be5b60103e14cd5ccea239
Shayesteh Arasti <shayesteharasti@gmail.com>
2020-07-19 11:30:54 +0430

    wtf

--- users/api/views.py
+++ users/api/views.py
@@ -51,7 +51,7 @@ class FratePostsView(ListCreateAPIView, UpdateAPIView):
             filename = filename,
             category = category,
             ratings = ratings,
-            count = ratec,
+            rateCount = ratec,
             caption = caption
             )
         m.save()
