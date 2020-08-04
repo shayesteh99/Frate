a1a29e3f9fe62a76e7726b3f1c23fa06432e08aa
Shayesteh Arasti <shayesteharasti@gmail.com>
2020-08-04 09:37:20 +0430

    new follower model

--- users/admin.py
+++ users/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from users.models import FrateUser, FratePost, FrateFollower, FrateComment
+from users.models import FrateUser, FratePost, FrateFollower, FrateComment, Follower
 
 # Register your models here.
 admin.site.register(FrateUser)
 admin.site.register(FratePost)
 admin.site.register(FrateFollower)
+admin.site.register(Follower)
 admin.site.register(FrateComment)
